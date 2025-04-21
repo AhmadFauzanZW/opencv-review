21b0e191de0e7c1456f64df966dea62ab2d8af67
AhmadFauzanZW <ahfa201204@gmail.com>
2025-04-21 11:37:58 +0700

    initial commit

--- Sesi1_Intro-OpenCV.py
+++ Sesi1_Intro-OpenCV.py
@@ -4,7 +4,7 @@ img = cv2.imread('orang.png', 0)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 img = cv2.rotate(img, cv2.ROTATE_180)
 
-cv2.imwrite("orang_kebalik.png", img)
+cv2.imwrite("orang_kebalik_grayscale_50%.png", img)
 
 cv2.imshow('orang', img)
 
